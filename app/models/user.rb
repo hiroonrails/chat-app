e0690a2c434a654718b070fb9ceeb7ab8391b501
hiroonrails <m69lover.scachaaan@gmail.com>
2021-02-15 13:15:30 +0900

    新規作成画面の表示(モデル全作成)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  has_many :room_users
+  has_many :rooms, through: :room_users
+  has_many :messages
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
