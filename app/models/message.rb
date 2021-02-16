298ea84b1fac08529391e671ae5884326e6b6a7e
hiroonrails <m69lover.scachaaan@gmail.com>
2021-02-16 14:48:23 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-
+  
   validates :content, presence: true, unless: :was_attached?
   def was_attached?
     self.image.attached?
